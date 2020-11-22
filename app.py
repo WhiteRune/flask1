34729eada53cdcb8694f8c89ce05123a94884e49
Stanislav <whiter.rune@gmail.com>
2020-11-22 19:55:32 +0300

    'complete'

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request
+from cryptography.fernet import Fernet
+
+app = Flask(__name__)
+
+key = b'bzQzIX4gp5dfBC79P5OLoqqJdwvA6yqJ2nvA3NLX4Us='
+f = Fernet(key)
+
+
+@app.route('/encrypt')
+def encrypt():
+    """
+    Shows text in encrypted view
+    """
+    string = request.args.get("string", 'None')
+    if string == 'None':
+        name = ''
+        token = "Произошли технические шоколадки, не обнаружена строка."
+    else:
+        name = 'Encrypted result:'
+        string = string.encode('utf-8')
+        token = (f.encrypt(string)).decode()
+    return render_template('index.html', result=token, name=name)
+
+
+@app.route('/decrypt')
+def decrypt():
+    """
+    Shows text in decrypted view
+    """
+    token = request.args.get("string", 'None')
+    if token == 'None':
+        name = ''
+        string = "Произошли технические шоколадки, не обнаружена строка."
+    else:
+        name = 'Decrypted result:'
+        token = token.encode('utf-8')
+        string = (f.decrypt(token)).decode()
+    return render_template('index.html', result=string, name=name)
+
+
+app.run(debug=True)
+
